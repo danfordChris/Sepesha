ee13776c469910947977bebeb63a17f6c1ff8f94
Moses M <mosesmberwa@gmail.com>
2024-12-30 10:36:55 +0300

    vehicle registration tools

--- migrations/m240914_170838_workflows_data.php
+++ migrations/m240914_170838_workflows_data.php
@@ -22,6 +22,7 @@ class m240914_170838_workflows_data extends Migration
             [
                 [1, 'Vendor Approval', 2, 'VENDOR APPROVAL', 1, '2024-09-07 19:44:40', 1, '2024-09-16 19:27:46', 1],
                 [2, 'Driver Onboarding', 2, 'DRIVER ONBOARDING', 1, '2024-09-14 19:32:09', NULL, NULL, 1],
+                [3, 'Vehicle Registration', 2, 'VEHICLE REGISTRATION', 1, '2024-09-14 19:32:09', NULL, NULL, 1],
             ]
         );
 
@@ -29,10 +30,13 @@ class m240914_170838_workflows_data extends Migration
             'wfstages',
             ['id', 'wid', 'sno', 'sname', 'actok', 'okchar', 'actnotok', 'notokchar', 'rptdisplay', 'status', 'cby', 'cdate', 'eby', 'edate', 'nextstage', 'backstage', 'okname', 'notokname'],
             [
-                [1, 1, 1, 'Request Verification', 'Ok,Submit', 'Y', 'No, Rectify', 'N', NULL, 'active', 1, '2024-09-07 19:47:25', NULL, NULL, 2, 1, 'Verified', 'Not Verified'],
+                [1, 1, 1, 'Request Verification', 'Ok,Verify', 'Y', 'No, Rectify', 'N', NULL, 'active', 1, '2024-09-07 19:47:25', NULL, NULL, 2, 1, 'Verified', 'Not Verified'],
                 [2, 1, 2, 'Review & Approval', 'Approve', 'Y', 'No, Rectify', 'N', NULL, 'active', 1, '2024-09-07 19:55:50', NULL, NULL, 3, 1, 'Approved', 'Not Approved'],
-                [3, 2, 1, 'Request Verification', 'Ok,Submit', 'Y', 'No, Rectify', 'N', NULL, 'active', 1, '2024-09-14 19:44:00', NULL, NULL, 2, 1, 'Verified', 'Not Verified'],
+                [3, 2, 1, 'Request Verification', 'Ok,Verify', 'Y', 'No, Rectify', 'N', NULL, 'active', 1, '2024-09-14 19:44:00', NULL, NULL, 2, 1, 'Verified', 'Not Verified'],
                 [4, 2, 2, 'Review & Approval', 'Approve', 'Y', 'No, Rectify', 'N', NULL, 'active', 1, '2024-09-14 19:48:29', NULL, NULL, 3, 1, 'Approved', 'Not Approved'],
+
+                [5, 3, 1, 'Request Verification', 'Ok,Verify', 'Y', 'No, Rectify', 'N', NULL, 'active', 1, '2024-09-14 19:44:00', NULL, NULL, 2, 1, 'Verified', 'Not Verified'],
+                [6, 3, 2, 'Review & Approval', 'Approve', 'Y', 'No, Rectify', 'N', NULL, 'active', 1, '2024-09-14 19:48:29', NULL, NULL, 3, 1, 'Approved', 'Not Approved'],
 
             ]
         );
