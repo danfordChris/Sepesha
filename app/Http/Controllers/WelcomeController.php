da9fabc2ead38d98862a3b1214fd9663f7aa7b65
Moses M <mosesmberwa@gmail.com>
2025-01-01 00:15:25 +0300

    welcome images

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -17,7 +17,7 @@ class WelcomeController extends Controller
 
     public function index(Request $request){
         try {
-            $data= Welcome::select('id','name as title', 'photo', 'type as reference','description')->get();;
+            $data= Welcome::select('id','name as title', 'photo', 'type as reference','description','order')->get();;
             if($data){
                 return CustomHelper::response(true, 'data found',200,$data);
             }else{
