09dbb7abe958c367e711625f2c9e1e5fcbf9de16
Moses M <mosesmberwa@gmail.com>
2025-01-04 21:29:01 +0300

    vehicles columns

---
+++ database/migrations/2025_01_04_180117_add_approval_columns_to_vehicles.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+            $table->foreignId('approved_by')->nullable()->constrained('clients_info');
+            $table->timestamp('approved_at')->nullable();
+            $table->decimal('latitude', 10, 8)->nullable();
+            $table->decimal('longitude', 11, 8)->nullable();
+            $table->timestamp('location_updated_at')->nullable();
+            $table->dateTime('assignment_start')->nullable();
+            $table->dateTime('assignment_end')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+            $table->dropColumn('approved_by');
+            $table->dropColumn('approved_at');
+            $table->dropColumn('latitude');
+            $table->dropColumn('longitude');
+            $table->dropColumn('location_updated_at');
+            $table->dropColumn('assignment_start');
+            $table->dropColumn('assignment_end');
+        });
+    }
+};
