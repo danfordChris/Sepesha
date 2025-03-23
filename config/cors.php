38c4cea836abeeea3334b8b610d7952f2de24259
Moses M <mosesmberwa@gmail.com>
2025-03-23 15:26:19 +0300

    cors

---
+++ config/cors.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'], // Define which routes CORS applies to
+    'allowed_methods' => ['*'], // Allow all HTTP methods
+    'allowed_origins' => ['*'], // Allow all origins
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'], // Allow all headers
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
+];
