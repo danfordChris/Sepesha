7fec2af61b281bde24464dfe8e6f3074276bcdfd
KAL99-byte <danielkalasa99@gmail.com>
2025-04-02 17:54:53 +0300

    updates on client infos details

--- views/client-infos/view.php
+++ views/client-infos/view.php
@@ -4,91 +4,101 @@ use yii\helpers\Html;
 use yii\widgets\DetailView;
 
 /** @var yii\web\View $this */
-/** @var app\models\ClientsInfo $model */
+/** @var app\models\Employee $model */
 
-$this->title = $model->name;
-$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Clients Infos'), 'url' => ['index']];
+$this->title = 'Client Infos  for:' .  $model->name;
+$this->params['breadcrumbs'][] = ['label' => 'Commissions', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
-<div class="clients-info-view">
+<div class="employee-view card">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="card-header bg-success">
+        <h5 class="mt-2 text-white"><?= Html::encode($this->title) ?></h5>
+    </div>
+    <div class="card-body">
 
-    <p>
-        <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                'method' => 'post',
+        <?= DetailView::widget([
+            'model' => $model,
+            'options' => ['class' => ' table table-responsive bordered table-sm'],
+            'attributes' => [
+                // 'id',
+                'role',
+                'entity_type',
+                'reference_number',
+                'name',
+                'mname',
+                'sname',
+                'email:email',
+                'phonecode',
+                'phone',
+                // 'password',
+                // 'password_hash',
+                // 'password_reset_token',
+                'company_id',
+                // 'confirmation_token',
+                // 'login_attempts',
+                // 'userid',
+                // 'auth_key',
+                // 'password_expiry',
+                'driver_license_number',
+                'license_expiry_date',
+                'rating',
+                'total_rides',
+                'total_ratings',
+                'total_deliveries',
+                'profile_photo',
+                'dob',
+                // 'is_verified',
+                // 'wallet_balance_tzs',
+                // 'wallet_balance_usd',
+                'preferred_payment_method',
+                'country_id',
+                'region_id',
+                'district_id',
+                'address',
+                'ward',
+                'street',
+                'house_number',
+                'postal_code',
+                'latitude',
+                'longitude',
+                'location_updated_at',
+                'status',
+                'attachment',
+                'approved_by',
+                'approved_date',
+                // 'wid',
+                // 'stid',
+                // 'wfstatus',
+                // 'requserinput',
+                'created_by',
+                'updated_by',
+                'deleted_at',
+                'created_at',
+                'updated_at',
+                // 'otp',
+                // 'otp_expires_at',
+                // 'privacy_checked',
+                'referal_code',
+
             ],
         ]) ?>
-    </p>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'role',
-            'entity_type',
-            'reference_number',
-            'name',
-            'mname',
-            'sname',
-            'email:email',
-            'phonecode',
-            'phone',
-            'password',
-            'password_hash',
-            'password_reset_token',
-            'company_id',
-            'confirmation_token',
-            'login_attempts',
-            'userid',
-            'auth_key',
-            'password_expiry',
-            'driver_license_number',
-            'license_expiry_date',
-            'rating',
-            'total_rides',
-            'total_ratings',
-            'total_deliveries',
-            'profile_photo',
-            'dob',
-            'is_verified',
-            'wallet_balance_tzs',
-            'wallet_balance_usd',
-            'preferred_payment_method',
-            'country_id',
-            'region_id',
-            'district_id',
-            'address',
-            'ward',
-            'street',
-            'house_number',
-            'postal_code',
-            'latitude',
-            'longitude',
-            'location_updated_at',
-            'status',
-            'attachment',
-            'approved_by',
-            'approved_date',
-            'wid',
-            'stid',
-            'wfstatus',
-            'requserinput',
-            'created_by',
-            'updated_by',
-            'deleted_at',
-            'created_at',
-            'updated_at',
-            'otp',
-            'otp_expires_at',
-            'privacy_checked',
-            'referal_code',
-        ],
-    ]) ?>
+        <?php
+        if ($model->role == 'driver') {
+            $url = ['client-infos/driver'];
+        } elseif ($model->role == 'customer') {
+            $url = ['client-infos/customer'];
+        } else {
+            $url = ['client-infos/vendor'];
+        }
 
+        echo Html::a(
+            '<i class="fa fa-arrow-left"></i> Back',
+            $url,
+            ['class' => 'btn btn-secondary']
+        );
+        ?>
+    </div>
 </div>
